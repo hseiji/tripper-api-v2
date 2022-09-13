7f7c5f9c937b40ed2997c7334d7e6b3766c06d96
hseiji <henrique.hst@gmail.com>
2022-09-13 07:57:03 -0400

    change index connection to heroku

--- src/controllers/events.js
+++ src/controllers/events.js
@@ -1,12 +1,15 @@
+const { Pool } = require('pg');
 const db = require('../db');
 
 exports.getEvents = async (req,res) => {
   try {
+    const client = await Pool.connect();
     const { rows } = await db.query('select * from events');
     return res.status(200).json({
       success: true,
       users: rows,
     })
+    client.release();
   } catch (error) {
     console.log(error.message);
   }
--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,18 @@
-const { Pool } = require('pg')
+const { Pool } = require('pg');
 const pool = new Pool({
-  host: 'localhost',
-  user: 'vagrant',
-  password: 'vagrant',
-  database: 'finaltripper',
-  port: 5432,
   connectionString: process.env.DATABASE_URL,
-})
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+// const pool = new Pool({
+//   host: 'localhost',
+//   user: 'vagrant',
+//   password: 'vagrant',
+//   database: 'finaltripper',
+//   port: 5432,
+// })
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
