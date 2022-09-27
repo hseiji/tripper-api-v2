da91c3808daa3a38339f54a08b240e7d368acc79
hseiji <henrique.hst@gmail.com>
2022-09-26 22:33:16 -0400

    add addNewUser

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,4 +1,5 @@
 const db = require('../db');
+const bcrypt = require('bcrypt')
 
 // Get all users
 exports.getUsers = async (req,res) => {
@@ -20,5 +21,19 @@ exports.getUserInfo = async (req,res) => {
     return res.status(200).json({ rows })
   } catch (error) {
     console.log(error.message);
+  }
+}
+
+// Add new user
+exports.addNewUser = async (req, res) => {
+  try {
+    let queryString = `INSERT INTO users (name, email, password, lat, lng) VALUES ($1, $2, $3, $4, $5);`;
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    let queryParams = [req.body.name, req.body.email, hashedPassword, "43.6532976025993", "-79.38359538925825"];
+    const { rows } = await db.query(queryString, queryParams)
+    return res.status(200).json({ rows })
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send();
   }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,9 @@
 const { Router } = require('express')
-const { getUsers, getUserInfo } = require('../controllers/users')
+const { getUsers, getUserInfo, addNewUser } = require('../controllers/users')
 const router = Router()
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUserInfo);
+router.post('/users/', addNewUser);
 
 module.exports = router
