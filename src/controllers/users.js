3b5c7f0b817aa8b38cbb651eaaebfab6ac695c5d
hseiji <henrique.hst@gmail.com>
2022-09-27 21:41:37 -0400

    add authUser

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -31,10 +31,35 @@ exports.addNewUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
     
     let queryParams = [req.body.name, req.body.email, hashedPassword, "43.6532976025993", "-79.38359538925825"];
-    const { rows } = await db.query(queryString, queryParams)
-    return res.status(200).json({ rows })
+    const { rows } = await db.query(queryString, queryParams);
+    return res.status(200).json({ rows });
   } catch (error) {
     console.log(error.message);
+    res.status(500).send("Something went wrong");
+  }
+}
+
+// Authenticate user
+exports.authUser = async (req, res) => {
+  try {
+    let queryString = `SELECT * FROM users WHERE users.email = $1`;
+    let queryParams = [req.body.email];
+    const { user } = await db.query(queryString, queryParams);
+    
+    console.log("user:", user);
+
+    //First check the username
+    if (user === null) {
+      res.status(400).json("Wrong credentials.");
+    } else {
+      //Now it checks the password
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        res.status(200).json("Login successful");
+      } else {
+        res.send("Not allowed")
+      }
+    }
+  } catch (error) {
     res.status(500).send("Something went wrong");
   }
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
-const { getUsers, getUserInfo, addNewUser } = require('../controllers/users')
+const { getUsers, getUserInfo, addNewUser, authUser } = require('../controllers/users')
 const router = Router()
 
 router.get('/users', getUsers);
 router.get('/users/:userId', getUserInfo);
 router.post('/users', addNewUser);
+router.post('/users/login', authUser);
 
 module.exports = router
