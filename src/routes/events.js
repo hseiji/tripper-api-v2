05ab8f6950b124207a1cc358c5adb223135eb11a
hseiji <henrique.hst@gmail.com>
2022-10-01 11:57:05 -0400

    change events routes

--- src/routes/events.js
+++ src/routes/events.js
@@ -1,12 +1,12 @@
 const { Router } = require('express')
 const router = Router()
-const { getEventsForPlan, addEventToPlan, deleteEvent, getEventById, markEventDone, getSearch, getEvents } = require('../controllers/events');
+const { getEventsForPlan, addEventToPlan, deleteEvent, getEventById, markEventDone, getSearch } = require('../controllers/events');
 // const { authToken } = require('../controllers/users');
 
-router.get('/events', getEvents);
-router.get('/events/:planId', getEventsForPlan);
+// router.get('/events', getEvents);
+// router.get('/events/:planId', getEventsForPlan);
 
-// router.get('/events/', authToken, getEventsForPlan);
+router.get('/events', authToken, getEventsForPlan);
 
 router.put('/events/:planId', addEventToPlan);
 router.get('/events/id/:eventId', getEventById);
