bcf705641b939dff183ad0b3d2acb578c00b4429
hseiji <henrique.hst@gmail.com>
2022-10-17 08:03:52 -0400

    clean comments

--- src/controllers/plans.js
+++ src/controllers/plans.js
@@ -6,15 +6,16 @@ exports.getPlans = async (req, res) => {
     const { rows } = await db.query(queryString)
     return res.status(200).json({ rows })    ;
   } catch (error) {
-    console.log(error.message);
+    return res.status(404).json({
+      message: "Error on getPlans: "
+    })
   }
 }
 
 exports.getPlansForUser = async (req, res) => {
   try {
-    // let queryString = `SELECT * FROM plans WHERE plans.user_id = $1;`;
+    console.log("On getPlansForUser...");
     let queryString = `SELECT * FROM plans WHERE plans.user_email = $1;`;
-    // let queryParams = [req.params.userId];
     let queryParams = [req.user.email];
     const { rows } = await db.query(queryString, queryParams)
     return res.status(200).json({ rows });
@@ -30,13 +31,14 @@ exports.getPlansForUser = async (req, res) => {
 exports.addNewPlan = async (req, res) => {
   try {
     let queryString = `INSERT INTO plans (user_id, name, ordering, user_email) VALUES ($1, $2, 1, $3);`;
-    console.log("req.body:", req.body);
+    console.log("on addNewPlan... req.body=", req.body);
     let queryParams = [req.body.info.userId, req.body.info.planName, req.body.info.userEmail];
-    // let queryParams = [user.user_id, req.body.info.planName, user.user_email]; // from jwt 
     const { rows } = await db.query(queryString, queryParams)
     return res.status(200).json({ rows })    
   } catch (error) {
-    console.log(error.message);
+    return res.status(404).json({
+      message: "Error on addNewPlan: "
+    })
   }
 }
 
@@ -48,6 +50,8 @@ exports.deletePlan = async (req, res) => {
     const { rows } = await db.query(queryString, queryParams)
     return res.status(200).json({ rows })       
   } catch (error) {
-    console.log(error);
+    return res.status(404).json({
+      message: "Error on deletePlan: "
+    })
   }
 }
